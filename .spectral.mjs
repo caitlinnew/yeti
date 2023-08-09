6f4f40ae274fddab63797530769bd18fc7806cf7
caitlin <caitlin@stoplight.io>
2023-08-09 07:11:34 -0600

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwOTU4NA/spectral.js?branch_slug=master&token=3c7f1d2e-d6fd-425d-abec-1c8b28095f5c";
+export default { extends: ruleset };
